cbb5be6489482a8fd58b3d718ac0037d17aaf674
Victor Martins <hidiku_lhiku@hotmail.com>
2020-12-06 20:48:09 -0300

    Atualização deploy

--- AgendamentoDeViagens/SistemaDeAgendamentoDeViagens/Startup.cs
+++ AgendamentoDeViagens/SistemaDeAgendamentoDeViagens/Startup.cs
@@ -33,12 +33,12 @@ namespace SistemaDeAgendamentoDeViagens
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-           
-                  
-            app.UseExceptionHandler("/Home/Error");
-              
-            app.UseHsts();
-            
+
+
+            app.UseDeveloperExceptionPage();
+
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
